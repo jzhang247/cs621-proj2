62881aa884eeb59da218e8ce381979b5f04b54f8
jzhang247 <jzhang247@dons.usfca.edu>
2025-05-06 01:31:51 -0700

    Minor tweak.

--- setup.bash
+++ setup.bash
@@ -1,10 +1,9 @@
 git clone https://gitlab.com/nsnam/ns-3-dev.git ns3
-(cd ns3 & git checkout -b ns-3.44-branch ns-3.44)
-cp -rf ./submission/* ./ns3/
 cd ns3
-pwd
+git checkout -b ns-3.44-branch ns-3.44
+cp -rf ../submission/* ./
 ./ns3 configure --enable-examples --enable-tests --cxx-standard 23
 cp ./cmake-cache/compile_commands.json ./compile_commands.json
 ./ns3 build
-# ./test.py
+./test.py
 ./ns3 run first
