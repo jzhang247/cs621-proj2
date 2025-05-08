864ac3a773c18c48613cacd1a77674ef79160146
jzhang247 <jzhang247@dons.usfca.edu>
2025-05-07 23:46:33 -0700

    Desktop/project2/proj2/setup.bash

--- setup.bash
+++ setup.bash
@@ -1,8 +1,20 @@
-git clone https://gitlab.com/nsnam/ns-3-dev.git ns3
+git clone https://github.com/jzhang247/ns3-fork.git ns3
 cd ns3
-git checkout -b ns-3.44-branch ns-3.44
-cp -rf ../submission/* ./
+git checkout -b dev
 
-./refresh.bash
+
+
+# git clone https://gitlab.com/nsnam/ns-3-dev.git ns3
+# cd ns3
+# git checkout -b ns-3.44-branch ns-3.44
+# cp -rf ../submission/* ./
+
+
+exit 0
+
+# ./ns3 configure --enable-examples --enable-tests --cxx-standard 23
+./ns3 configure --enable-examples --enable-tests
+cp ./cmake-cache/compile_commands.json ./compile_commands.json
+./ns3 build
 ./test.py
 ./ns3 run first
